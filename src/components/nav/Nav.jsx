c842b692bfbf432c2a67a44f4402261fff546ff1
mburrow303 <elio21@comcast.net>
2023-11-27 21:23:44 -0700

    added Nav file & folder

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {Navbar} from "reactstrap";
+import {Link} from "react-router-dom";
+
+const Nav = ({ token, clearToken }) => {
+  return (
+    <nav>
+      <Navbar className="my-2" color="secondary">
+        <NavbarBrand>
+          {/* {!token ? (
+            <Link style={{ margin: "3px 10px" }} to="/">
+              Sign Up/Login
+            </Link>
+          ) : (
+            <Link onClick={clearToken} style={{ margin: "3px 10px" }} to="/">
+              Log Out
+            </Link>
+          )} */}
+          <h2>
+            <b>Thyme Share</b>
+          </h2>
+        </NavbarBrand>
+      </Navbar>
+    </nav>
+  );
+};
+
+export default Nav;
