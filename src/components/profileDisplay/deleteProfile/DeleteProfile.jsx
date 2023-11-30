5ef04e4a3205fe24040febb338ff1bff8699565d
mburrow303 <elio21@comcast.net>
2023-11-29 20:54:17 -0700

    worked on Update & Delete Profile

--- src/components/profileDisplay/deleteProfile/DeleteProfile.jsx
+++ src/components/profileDisplay/deleteProfile/DeleteProfile.jsx
@@ -1,3 +1,34 @@
+import React, { useState } from "react";
 
+function DeleteProfile() {
+  const [profile, setProfile] = useState("");
+
+  const [response, setResponse] = useState("");
+
+  const handleInputChange = async (e) => {
+    e.preventDefault();
+
+    try {
+      let response = await fetch("http://127.0.0.1:4000/profile/:username", {
+        method: "DELETE",
+      });
+
+      const data = await response.json();
+      setResponse(data.profile);
+    } catch (err) {
+      console.error("Error:", err);
+    }
+  };
+
+  return (
+    <div className="delete-profile">
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={profile} onChange={handleInputChange} />
+        <button type="submit">Delete Profile</button>
+      </form>
+      {response && <p>{response}</p>}
+    </div>
+  );
+}
 
 export default DeleteProfile;
