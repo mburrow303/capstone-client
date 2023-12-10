1b6343378194d27aed8e7399adda6f074eb5fdb1
Irina001 <37217511+Irina001@users.noreply.github.com>
2023-12-10 15:46:05 -0700

    Update Signup.jsx
    added import ./signup.css

--- src/components/auth/signup/Signup.jsx
+++ src/components/auth/signup/Signup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import './signup.css';
 
 function Signup(props) {
   const navigate = useNavigate("/profile");
